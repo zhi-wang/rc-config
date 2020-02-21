8765738c263a0019b7e0b9e59c1e682ce0648d6c
Zhi Wang <oukore@gmail.com>
2020-02-21 15:50:50 -0600

    added texlive

--- bash-vars.sh
+++ bash-vars.sh
@@ -5,6 +5,12 @@
 __append_dir_to_back $HOME/local/arsenal PATH
 
 
+# texlive 2019
+__insert_dir_to_front /usr/local/texlive/2019/texmf-dist/doc/man MANPATH
+__insert_dir_to_front /usr/local/texlive/2019/texmf-dist/doc/info INFOPATH
+__insert_dir_to_front /usr/local/texlive/2019/texmf-dist/doc/info PATH
+
+
 # cuda
 if __linux; then
   __insert_dir_to_front /usr/local/cuda/bin PATH
@@ -14,4 +20,9 @@ fi
 if __linux; then
   __append_dir_to_back /opt/pgi/linux86-64-llvm/2019/bin PATH
   __append_dir_to_back /opt/pgi/linux86-64-llvm/2019/lib LD_LIBRARY_PATH
+fi
+# openmm
+if __linux; then
+  __insert_dir_to_front /usr/local/openmm/lib/plugins OPENMM_PLUGIN_DIR
+  __insert_dir_to_front /usr/local/openmm/lib LD_LIBRARY_PATH
 fi
