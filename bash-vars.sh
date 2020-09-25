7b8f8d7de4dac2ea956f7267d2c87288f89b777b
Zhi Wang <oukore@gmail.com>
2020-09-25 17:48:09 -0500

    add intel mkl

--- bash-vars.sh
+++ bash-vars.sh
@@ -46,6 +46,10 @@ if __linux; then
   if [ -d /opt/intel/parallel_studio_xe_2019/bin ]; then
     source /opt/intel/parallel_studio_xe_2019/bin/psxevars.sh &> /dev/null
   fi
+  if [ -d /opt/intel/mkl ]; then
+    export MKLROOT=/opt/intel/mkl
+    __append_dir_back "$MKLROOT/lib/intel64" LD_LIBRARY_PATH
+  fi
 fi
 
 
