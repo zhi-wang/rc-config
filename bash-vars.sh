ad255fc8d6560d4accb347ebe40f88bcf93cc179
Zhi Wang <oukore@gmail.com>
2021-01-19 15:27:22 -0600

    add PGI 20.11

--- bash-vars.sh
+++ bash-vars.sh
@@ -76,6 +76,10 @@ if [ -d /opt/nvidia/hpc_sdk/Linux_x86_64/20.9 ]; then
   PGI=/opt/nvidia/hpc_sdk/Linux_x86_64/20.9/compilers
   PGIMPI=/opt/nvidia/hpc_sdk/Linux_x86_64/20.9/comm_libs/mpi
 fi
+if [ -d /opt/nvidia/hpc_sdk/Linux_x86_64/20.11 ]; then
+  PGI=/opt/nvidia/hpc_sdk/Linux_x86_64/20.11/compilers
+  PGIMPI=/opt/nvidia/hpc_sdk/Linux_x86_64/20.11/comm_libs/mpi
+fi
 if __linux; then
   __append_dir_back "$PGI/bin" PATH
   __append_dir_back "$PGI/lib" LD_LIBRARY_PATH
