b780ee722e32072985425c70aa92234db4aa6760
Zhi Wang <oukore@gmail.com>
2022-03-07 17:48:57 -0600

    Add nvhpc 22.2

--- v2/70-pgi.sh
+++ v2/70-pgi.sh
@@ -1,9 +1,12 @@
-if [ -d /opt/nvidia/hpc_sdk/Linux_x86_64/20.11 ]; then
-    PGI=/opt/nvidia/hpc_sdk/Linux_x86_64/20.11/compilers
-    PGIMPI=/opt/nvidia/hpc_sdk/Linux_x86_64/20.11/comm_libs/mpi
+if [ -d /opt/nvidia/hpc_sdk/Linux_x86_64/22.2 ]; then
+    PGI=/opt/nvidia/hpc_sdk/Linux_x86_64/22.2/compilers
+    PGIMPI=/opt/nvidia/hpc_sdk/Linux_x86_64/22.2/comm_libs/mpi
 elif [ -d /opt/nvidia/hpc_sdk/Linux_x86_64/21.7 ]; then
     PGI=/opt/nvidia/hpc_sdk/Linux_x86_64/21.7/compilers
     PGIMPI=/opt/nvidia/hpc_sdk/Linux_x86_64/21.7/comm_libs/mpi
+elif [ -d /opt/nvidia/hpc_sdk/Linux_x86_64/20.11 ]; then
+    PGI=/opt/nvidia/hpc_sdk/Linux_x86_64/20.11/compilers
+    PGIMPI=/opt/nvidia/hpc_sdk/Linux_x86_64/20.11/comm_libs/mpi
 elif [ -d /opt/nvidia/hpc_sdk/Linux_x86_64/20.9 ]; then
     PGI=/opt/nvidia/hpc_sdk/Linux_x86_64/20.9/compilers
     PGIMPI=/opt/nvidia/hpc_sdk/Linux_x86_64/20.9/comm_libs/mpi
