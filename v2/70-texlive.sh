40a7bc761dfd0f9d09369caf1a3019901ca33512
Zhi Wang <oukore@gmail.com>
2022-05-02 17:45:42 -0500

    update

--- v2/70-texlive.sh
+++ v2/70-texlive.sh
@@ -1,14 +1,19 @@
-if __linux; then
-      if [ -d /usr/local/texlive/2022 ]; then
-        TEXLIVE=/usr/local/texlive/2022
-    elif [ -d /usr/local/texlive/2021 ]; then
-        TEXLIVE=/usr/local/texlive/2021
-    elif [ -d /usr/local/texlive/2020 ]; then
-        TEXLIVE=/usr/local/texlive/2020
-    elif [ -d /usr/local/texlive/2019 ]; then
-        TEXLIVE=/usr/local/texlive/2019
-    fi
+  if [ -d /usr/local/texlive/2022 ]; then
+    TEXLIVE=/usr/local/texlive/2022
+elif [ -d /usr/local/texlive/2021 ]; then
+    TEXLIVE=/usr/local/texlive/2021
+elif [ -d /usr/local/texlive/2020 ]; then
+    TEXLIVE=/usr/local/texlive/2020
+elif [ -d /usr/local/texlive/2019 ]; then
+    TEXLIVE=/usr/local/texlive/2019
+fi
+
+if [ ! -z "$TEXLIVE" ]; then
     __insert_paths INFOPATH "$TEXLIVE/texmf-dist/doc/info"
     __insert_paths MANPATH  "$TEXLIVE/texmf-dist/doc/man"
+if __linux; then
     __insert_paths PATH     "$TEXLIVE/bin/x86_64-linux"
+elif __macos; then
+    __insert_paths PATH     "$TEXLIVE/bin/universal-darwin"
+fi
 fi
