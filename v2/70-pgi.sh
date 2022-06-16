42319046c11c34a9310ad4e30c6f4c7d3dbadcf2
Zhi Wang <oukore@gmail.com>
2022-06-16 05:45:25 -0500

    update PGI

--- v2/70-pgi.sh
+++ v2/70-pgi.sh
@@ -4,6 +4,9 @@
 elif [ -d /opt/nvidia/hpc_sdk/Linux_x86_64/21.7 ]; then
     PGI=/opt/nvidia/hpc_sdk/Linux_x86_64/21.7/compilers
     PGIMPI=/opt/nvidia/hpc_sdk/Linux_x86_64/21.7/comm_libs/mpi
+elif [ -d /opt/nvidia/hpc_sdk/Linux_x86_64/21.1 ]; then
+    PGI=/opt/nvidia/hpc_sdk/Linux_x86_64/21.1/compilers
+    PGIMPI=/opt/nvidia/hpc_sdk/Linux_x86_64/21.1/comm_libs/mpi
 elif [ -d /opt/nvidia/hpc_sdk/Linux_x86_64/20.11 ]; then
     PGI=/opt/nvidia/hpc_sdk/Linux_x86_64/20.11/compilers
     PGIMPI=/opt/nvidia/hpc_sdk/Linux_x86_64/20.11/comm_libs/mpi
@@ -19,7 +22,7 @@ if __linux; then
     __append_paths PATH            "$PGI/bin"
     __append_paths LD_LIBRARY_PATH "$PGI/lib"
     __append_paths MANPATH         "$PGI/man"
-    __append_paths PATH            "$PGIMPI/bin"
-    __append_paths LD_LIBRARY_PATH "$PGIMPI/lib"
-    __append_paths MANPATH         "$PGIMPI/share/man"
+    __insert_paths PATH            "$PGIMPI/bin"
+    __insert_paths LD_LIBRARY_PATH "$PGIMPI/lib"
+    __insert_paths MANPATH         "$PGIMPI/share/man"
 fi
